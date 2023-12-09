88fa093418f0c528141f5900674896757206a0eb
marcusp619 <marcusp619@gmail.com>
2023-12-09 16:22:55 -0500

    feat: handle post fn

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use postgres::Error as PostgresError;
 use postgres::{Client, NoTls};
 use std::env;
 use std::io::{Read, Write};
-use std::net::{TcpListener, Tcptream};
+use std::net::{TcpListener, TcpStream};
 
 #[macro_use]
 extern crate serde_derive;
@@ -30,6 +30,69 @@ fn main() {
         println!("Error: {}", e);
         return;
     }
+
+    //Start server
+    let listener = TcpListener::bind(format!(0.0.0.0:8080)).unwrap();
+    println!("Server started at port 8080");
+
+    //Handle the client
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
+
+//handle_client fn
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0, 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+            let (status_line, content) = match &*request {
+                r if request_with("POST /users") => handle_post_request(r),
+                r if request_with("GET /users/") => handle_get_request(r),
+                r if request_with("GET /users") => handle_get_all_request(r),
+                r if request_with("PUT /users/") => handle_put_request(r),
+                r if request_with("DELETE /users/") => handle_delete_request(r),
+                _ => (NOT_FOUND, "Not Found".to_string()),
+            };
+
+            stream
+                .write_all(format!("{}{}", status_line, content).as_bytes())
+                .unwrap();
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+}
+
+//CONTROLLERS
+//handle_post_request function
+fn handle_post_request(request: &str) -> (String, String) {
+    match (
+        get_user_request_body(&request),
+        Client::connect(DB_URL, NoTls),
+    ) {
+        (Ok(user), Ok(mut client)) => {
+            client
+                .execute(
+                    "INSERT INTO users (name, email) VALUES ($1, $2)",
+                    &[&user.name, &user.email],
+                )
+                .unwrap();
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
 }
 
 //set_database fn
@@ -46,4 +109,20 @@ fn set_database() -> Resul<(), PostgresError> {
             )",
         &[],
     )?;
+}
+
+//get_id fn
+fn get_id(request: &str) -> &str {
+    request
+        .split("/")
+        .nth(2)
+        .unwrap_or_default()
+        .split_whitespace()
+        .next()
+        .unwrap_or_default();
+}
+
+//deserialize user from request body with the id
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
 }
