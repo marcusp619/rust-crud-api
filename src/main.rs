fa57fd42a1eb7c8f7f68409b0b149577d041987d
marcusp619 <marcusp619@gmail.com>
2023-12-11 09:13:54 -0500

    feat: add rest of controllers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use postgres::Error as PostgresError;
 use postgres::{Client, NoTls};
+use postgres::{Error as PostgresError, GenericClient};
 use std::env;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
@@ -16,7 +16,7 @@ struct User {
 }
 
 //DATABASE_URL
-const DB_URL: &str = !env("DATABASE_URL");
+const DB_URL: &str = env!("DATABASE_URL");
 
 //constants
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
@@ -32,7 +32,7 @@ fn main() {
     }
 
     //Start server
-    let listener = TcpListener::bind(format!(0.0.0.0:8080)).unwrap();
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
     println!("Server started at port 8080");
 
     //Handle the client
@@ -48,9 +48,9 @@ fn main() {
     }
 }
 
-//handle_client fn
+//handle_client function
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0, 1024];
+    let mut buffer = [0; 1024];
     let mut request = String::new();
 
     match stream.read(&mut buffer) {
@@ -58,12 +58,12 @@ fn handle_client(mut stream: TcpStream) {
             request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
 
             let (status_line, content) = match &*request {
-                r if request_with("POST /users") => handle_post_request(r),
-                r if request_with("GET /users/") => handle_get_request(r),
-                r if request_with("GET /users") => handle_get_all_request(r),
-                r if request_with("PUT /users/") => handle_put_request(r),
-                r if request_with("DELETE /users/") => handle_delete_request(r),
-                _ => (NOT_FOUND, "Not Found".to_string()),
+                r if r.starts_with("POST /users") => handle_post_request(r),
+                r if r.starts_with("GET /users/") => handle_get_request(r),
+                r if r.starts_with("GET /users") => handle_get_all_request(r),
+                r if r.starts_with("PUT /users/") => handle_put_request(r),
+                r if r.starts_with("DELETE /users/") => handle_delete_request(r),
+                _ => (NOT_FOUND.to_string(), "404 Not Found".to_string()),
             };
 
             stream
@@ -90,25 +90,120 @@ fn handle_post_request(request: &str) -> (String, String) {
                     &[&user.name, &user.email],
                 )
                 .unwrap();
+
+            (OK_RESPONSE.to_string(), "User created".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
+}
+
+//handle_get_request function
+fn handle_get_request(request: &str) -> (String, String) {
+    match (
+        get_id(&request).parse::<i32>(),
+        Client::connect(DB_URL, NoTls),
+    ) {
+        (Ok(id), Ok(mut client)) => {
+            match client.query_one("SELECT * FROM users WHERE id = $1", &[&id]) {
+                Ok(row) => {
+                    let user = User {
+                        id: row.get(0),
+                        name: row.get(1),
+                        email: row.get(2),
+                    };
+
+                    (
+                        OK_RESPONSE.to_string(),
+                        serde_json::to_string(&user).unwrap(),
+                    )
+                }
+                _ => (NOT_FOUND.to_string(), "User not found".to_string()),
+            }
+        }
+
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
+}
+
+//handle_get_all_request function
+fn handle_get_all_request(request: &str) -> (String, String) {
+    match Client::connect(DB_URL, NoTls) {
+        Ok(mut client) => {
+            let mut users = Vec::new();
+
+            for row in client.query("SELECT * FROM users", &[]).unwrap() {
+                users.push(User {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                });
+            }
+
+            (
+                OK_RESPONSE.to_string(),
+                serde_json::to_string(&users).unwrap(),
+            )
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
+}
+
+//handle_put_request function
+fn handle_put_request(request: &str) -> (String, String) {
+    match (
+        get_id(&request).parse::<i32>(),
+        get_user_request_body(&request),
+        Client::connect(DB_URL, NoTls),
+    ) {
+        (Ok(id), Ok(user), Ok(mut client)) => {
+            client
+                .execute(
+                    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+                    &[&user.name, &user.email, &id],
+                )
+                .unwrap();
+
+            (OK_RESPONSE.to_string(), "User updated".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
+}
+
+//handle_delete_request function
+fn handle_delete_request(request: &str) -> (String, String) {
+    match (
+        get_id(&request).parse::<i32>(),
+        Client::connect(DB_URL, NoTls),
+    ) {
+        (Ok(id), Ok(mut client)) => {
+            let rows_affected = client
+                .execute("DELETE FROM users WHERE id = $1", &[&id])
+                .unwrap();
+
+            if rows_affected == 0 {
+                return (NOT_FOUND.to_string(), "User not found".to_string());
+            }
+
+            (OK_RESPONSE.to_string(), "User deleted".to_string())
         }
         _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
     }
 }
 
 //set_database fn
-fn set_database() -> Resul<(), PostgresError> {
+fn set_database() -> Result<(), PostgresError> {
     //Connect to database
     let mut client = Client::connect(DB_URL, NoTls)?;
 
     //Create table
-    client.execute(
+    client.batch_execute(
         "CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             name VARCHAR NOT NULL,
             email VARCHAR NOT NULL
             )",
-        &[],
     )?;
+    Ok(())
 }
 
 //get_id fn
@@ -119,7 +214,7 @@ fn get_id(request: &str) -> &str {
         .unwrap_or_default()
         .split_whitespace()
         .next()
-        .unwrap_or_default();
+        .unwrap_or_default()
 }
 
 //deserialize user from request body with the id
